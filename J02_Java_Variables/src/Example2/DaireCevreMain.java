f5e78eb0ea3c67c4fc59f719fa4c69da71e6cadc
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-07 16:17:52 +0300

    Variables

---
+++ J02_Java_Variables/src/Example2/DaireCevreMain.java
@@ -0,0 +1,21 @@
+package Example2;
+
+public class DaireCevreMain {
+
+	public static void main(String[] args) {
+
+		// Değişkenler
+		int yaricap = 7;
+		double pi = 3.14159;
+		double cevre;
+
+		// Çevreyi hesapla
+		cevre = 2 * pi * yaricap;
+
+		// Değişkenleri yazdır
+		System.out.println("Yarıçap: " + yaricap);
+		System.out.println("Pi: " + pi);
+		System.out.println("Dairenin Çevresi: " + cevre);
+	}
+
+}
