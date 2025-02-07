0f9e57025fc95e16e8e27e7e7ce5aa94eaa7e643
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-07 23:49:25 +0300

    DataTypes

---
+++ J03_Java_DataTypes/src/Example1/VeriTipleri.java
@@ -0,0 +1,19 @@
+package Example1;
+
+public class VeriTipleri {
+
+	public static void main(String[] args) {
+		// Farklı veri türlerinde değişkenler oluştur
+		int elmaSayisi = 30;
+		float elmaFiyati = 3.75f;
+		float toplamFiyat = elmaSayisi * elmaFiyati;
+		char paraBirimi = '₺';
+
+		// Değişkenleri yazdır
+		System.out.println("Elma sayısı: " + elmaSayisi);
+		System.out.println("Elma fiyatı: " + elmaFiyati + paraBirimi);
+		System.out.println("Toplam fiyat = " + toplamFiyat + paraBirimi);
+
+	}
+
+}
