e64e28b5600cb2d6e00b94c25ee4b4d36858aa13
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-07 15:00:06 +0300

    Variables

---
+++ J02_Java_Variables/src/PersonelMain.java
@@ -0,0 +1,19 @@
+
+public class PersonelMain {
+
+	public static void main(String[] args) {
+		String calisanAdi = "Ahmet Yilmaz";
+		int calisanNumarasi = 67890;
+		int calisanYasi = 35;
+		double calisanMaasi = 33500.75;
+		char calisanDerecesi = 'B';
+
+		// Değişkenleri yazdır
+		System.out.println("Çalışan Adı: " + calisanAdi);
+		System.out.println("Çalışan Numarası: " + calisanNumarasi);
+		System.out.println("Çalışan Yaşı: " + calisanYasi);
+		System.out.println("Çalışan Maaşı: " + calisanMaasi);
+		System.out.println("Çalışan Derecesi: " + calisanDerecesi);
+	}
+
+}
