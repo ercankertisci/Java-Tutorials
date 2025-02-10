c38fc216ea7c0e1a3fd4f9846d581d43c39bd233
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-10 16:02:01 +0300

    While Loop

---
+++ J09_Java_WhileLoop/src/example1/SayiMain.java
@@ -0,0 +1,14 @@
+package example1;
+
+public class SayiMain {
+
+	public static void main(String[] args) {
+
+		int sayi = 1;
+
+		while (sayi <= 10) {
+			System.out.println(sayi);
+			sayi++; // sayaç değişkenini bir artırıyoruz
+		}
+	}
+}
