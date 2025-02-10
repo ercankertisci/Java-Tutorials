020d1ab9f848139458260a4ab1c2418d16c99d71
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-10 10:09:48 +0300

    Operators

---
+++ J05_Java_Operators/src/ex1_assignment_operator/AssignmentOperatorMain.java
@@ -0,0 +1,20 @@
+package ex1_assignment_operator;
+
+public class AssignmentOperatorMain {
+
+	public static void main(String[] args) {
+		String ad = "Ercan";
+		int a = 7;
+		int c = a;
+
+		/*
+		 * a degiskenine 7 degerini atadık, daha sonra 7 olan a degiskenini c
+		 * degiskenine atadik
+		 */
+
+		System.out.println(ad);
+		System.out.println(a);
+		System.out.println(c);
+	}
+
+}
