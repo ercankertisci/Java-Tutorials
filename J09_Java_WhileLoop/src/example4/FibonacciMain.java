481c6987e7ea2ad5bbf85e6a7f9e806eec2189e6
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-10 16:20:14 +0300

    While Loop

---
+++ J09_Java_WhileLoop/src/example4/FibonacciMain.java
@@ -0,0 +1,19 @@
+package example4;
+
+public class FibonacciMain {
+
+	public static void main(String[] args) {
+		int n1 = 0, n2 = 1, n3, i = 2;
+		int limit = 10;
+
+		System.out.print(n1 + " " + n2);
+
+		while (i < limit) {
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+			i++;
+		}
+	}
+}
