ec714981980804588b5c79fa9d4d0d76f2e5fd4f
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-10 11:33:45 +0300

    Conditions

---
+++ J07_Java_Conditions/src/example2/SayiMain.java
@@ -0,0 +1,14 @@
+package example2;
+
+public class SayiMain {
+
+	public static void main(String[] args) {
+		int sayi = 74;
+
+		if (sayi < 50) {
+			System.out.println("Sayı 50'den küçük");
+		} else {
+			System.out.println("Sayı 50'den büyük");
+		}
+	}
+}
