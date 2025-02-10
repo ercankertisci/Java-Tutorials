67f2f5202b315a0c7b1e4cb1e66512d9400e2f18
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-10 10:24:03 +0300

    Operators

---
+++ J05_Java_Operators/src/ex5_logical_operator/LogicalOperator.java
@@ -0,0 +1,16 @@
+package ex5_logical_operator;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		boolean x = false;
+		boolean y = true;
+
+		System.out.println("x&&y=" + (x && y));
+		System.out.println("x||y=" + (x || y));
+		System.out.println("x^y=" + (x ^ y));
+		System.out.println("!x=" + (!x));
+
+	}
+
+}
