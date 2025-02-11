bce9ffcb2471c38e7a23205d70b8db4b9541b12c
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-11 12:17:50 +0300

    Do While Loop

---
+++ J10_Java_DoWhileLoop/src/example1/SayiMain.java
@@ -0,0 +1,13 @@
+package example1;
+
+public class SayiMain {
+
+	public static void main(String[] args) {
+		int sayi = 1;
+
+		do {
+			System.out.println(sayi);
+			sayi++;
+		} while (sayi <= 5);
+	}
+}
