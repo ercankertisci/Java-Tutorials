7a33b48757d8698cfb2ed8a04033dc6346c5cddc
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 11:28:11 +0300

    Methods

---
+++ J13_Java_Methods/src/example3/DegerDondurenMetotMain.java
@@ -0,0 +1,12 @@
+package example3;
+
+public class DegerDondurenMetotMain {
+    static int deger() {
+        return 5;
+    }
+
+    public static void main(String[] args) {
+        int sonuc = deger();
+        System.out.println("Döndürülen Değer: " + sonuc);
+    }
+}
