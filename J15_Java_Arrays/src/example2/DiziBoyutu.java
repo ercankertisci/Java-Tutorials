afda5d74e8227bc25ea813e28a3aec93e80bf6be
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 13:13:36 +0300

    Arrays

---
+++ J15_Java_Arrays/src/example2/DiziBoyutu.java
@@ -0,0 +1,8 @@
+package example2;
+
+public class DiziBoyutu {
+	public static void main(String[] args) {
+		int[] gunler = new int[7];
+		System.out.println("Dizinin Boyutu: " + gunler.length);
+	}
+}
