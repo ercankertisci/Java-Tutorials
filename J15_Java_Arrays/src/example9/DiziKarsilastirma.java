3dc812d5868bce9569b0ffba80f34ebd7bd9a894
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 13:31:48 +0300

    Arrays

---
+++ J15_Java_Arrays/src/example9/DiziKarsilastirma.java
@@ -0,0 +1,12 @@
+package example9;
+
+import java.util.Arrays;
+
+public class DiziKarsilastirma {
+	public static void main(String[] args) {
+		int[] dizi1 = { 1, 2, 3 };
+		int[] dizi2 = { 1, 2, 3 };
+
+		System.out.println(Arrays.equals(dizi1, dizi2));
+	}
+}
