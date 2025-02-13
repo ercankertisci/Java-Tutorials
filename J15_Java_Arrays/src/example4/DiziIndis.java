13ec3f3956ef2e6dac32c3b334bc8f50ecff8040
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 13:17:06 +0300

    Arrays

---
+++ J15_Java_Arrays/src/example4/DiziIndis.java
@@ -0,0 +1,8 @@
+package example4;
+
+public class DiziIndis {
+	public static void main(String[] args) {
+		int[] arrays = { 10, 20, 30, 40, 50 };
+		System.out.println("3. İndisin Değeri: " + arrays[3]);
+	}
+}
