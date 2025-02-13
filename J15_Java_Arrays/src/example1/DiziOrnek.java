5129b9457fff2a25fb26152fa0819ef85f058c36
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 13:11:57 +0300

    Arrays

---
+++ J15_Java_Arrays/src/example1/DiziOrnek.java
@@ -0,0 +1,10 @@
+package example1;
+
+public class DiziOrnek {
+	public static void main(String[] args) {
+		int[] gunler = { 1, 2, 3, 4, 5, 6, 7 };
+		for (int i = 0; i < gunler.length; i++) {
+			System.out.println("İndis: " + i + " Değer: " + gunler[i]);
+		}
+	}
+}
