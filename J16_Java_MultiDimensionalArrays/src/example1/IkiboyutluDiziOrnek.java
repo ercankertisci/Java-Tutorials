8f1dcaa6f6bd41fe639dbf63e523a0d667dd20e7
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 14:04:49 +0300

    MultiDimensional Arrays

---
+++ J16_Java_MultiDimensionalArrays/src/example1/IkiboyutluDiziOrnek.java
@@ -0,0 +1,12 @@
+package example1;
+
+public class IkiboyutluDiziOrnek {
+	public static void main(String[] args) {
+		int[][] dizi = { { 1, 7 }, { 5, 9 } };
+
+		System.out.println(dizi[0][0]); // 1. satır 1. sütun
+		System.out.println(dizi[0][1]); // 1. satır 2. sütun
+		System.out.println(dizi[1][0]); // 2. satır 1. sütun
+		System.out.println(dizi[1][1]); // 2. satır 2. sütun
+	}
+}
