06d6e4f51fdaad9058607f5a4a3426f08bbdbca1
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 13:15:26 +0300

    Arrays

---
+++ J15_Java_Arrays/src/example3/DiziListeleme.java
@@ -0,0 +1,10 @@
+package example3;
+
+public class DiziListeleme {
+	public static void main(String[] args) {
+		int[] sayilar = { 10, 20, 30, 40, 50 };
+		for (int i = 0; i < sayilar.length; i++) {
+			System.out.println("Eleman: " + sayilar[i]);
+		}
+	}
+}
