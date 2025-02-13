3c8d2196c0f90ef991eb66c8b806b14e7aa467f7
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 09:46:08 +0300

    Methods

---
+++ J13_Java_Methods/src/example1/ParametresizMetotlarMain.java
@@ -0,0 +1,11 @@
+package example1;
+
+public class ParametresizMetotlarMain {
+    static void metinYaz() {
+        System.out.println("Metot çağrılıyor");
+    }
+
+    public static void main(String[] args) {
+        metinYaz(); // Metod çağrılıyor
+    }
+}
