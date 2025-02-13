7efa44d8a5fbd67fd8776301efae6f885d1da2bc
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-13 09:49:26 +0300

    Methods

---
+++ J13_Java_Methods/src/example2/IslemMetotlariMain.java
@@ -0,0 +1,26 @@
+package example2;
+
+public class IslemMetotlariMain {
+    static void topla() {
+        System.out.println("Toplama Sonucu: " + (15 + 23));
+    }
+
+    static void cikarma() {
+        System.out.println("Çıkarma Sonucu: " + (7 - 3));
+    }
+
+    static void carpma() {
+        System.out.println("Çarpma Sonucu: " + (4 * 5));
+    }
+
+    static void bolme() {
+        System.out.println("Bölme Sonucu: " + (20 / 4));
+    }
+
+    public static void main(String[] args) {
+        topla();
+        cikarma();
+        carpma();
+        bolme();
+    }
+}
