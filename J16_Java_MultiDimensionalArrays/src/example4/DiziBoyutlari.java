5827a4307732ca74b600662bceddddb6e2619883
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-14 10:14:53 +0300

    MultiDimensional Arrays

---
+++ J16_Java_MultiDimensionalArrays/src/example4/DiziBoyutlari.java
@@ -0,0 +1,13 @@
+package example4;
+
+public class DiziBoyutlari {
+	public static void main(String[] args) {
+		int[][] dizi = { { 13, 14 }, { 5, 6, 9 }, { 1 }, { 4, 8, 14, 67 } };
+
+		System.out.println("Dizinin Boyutu: " + dizi.length);
+		System.out.println("0. İndisin Eleman Sayısı: " + dizi[0].length);
+		System.out.println("1. İndisin Eleman Sayısı: " + dizi[1].length);
+		System.out.println("2. İndisin Eleman Sayısı: " + dizi[2].length);
+		System.out.println("3. İndisin Eleman Sayısı: " + dizi[3].length);
+	}
+}
