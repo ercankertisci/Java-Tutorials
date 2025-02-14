f331dfb12229ca52270eb9c54ae329fac971edcd
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-14 15:21:09 +0300

    Encapsulation

---
+++ J20_Java_Encapsulation/src/example6/Test2.java
@@ -0,0 +1,11 @@
+package example6;
+
+//import example5.Hayvan;
+
+public class Test2 {
+    public static void main(String[] args) {
+//        Hayvan kedi = new Hayvan(); // Hata: Hayvan sınıfına erişilemez
+//        kedi.isim = "Tekir";        // Hata: isim değişkenine erişilemez
+//        kedi.sesCikar();            // Hata: sesCikar() metoduna erişilemez
+    }
+}
