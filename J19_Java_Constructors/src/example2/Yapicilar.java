4dcf9b09ecc1ce731f479914d39ed33533b925db
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-14 14:46:48 +0300

    Constructors

---
+++ J19_Java_Constructors/src/example2/Yapicilar.java
@@ -0,0 +1,9 @@
+package example2;
+
+public class Yapicilar {
+	int sayi;
+
+	public Yapicilar(int deger) { // Parametreli constructor
+		sayi = deger; // Parametreden gelen değeri atıyoruz.
+	}
+}
---
+++ J19_Java_Constructors/src/example2/YapicilarMain.java
@@ -0,0 +1,9 @@
+package example2;
+
+public class YapicilarMain {
+
+	public static void main(String[] args) {
+		Yapicilar islem2 = new Yapicilar(50); // 50 değerini gönderiyoruz.
+		System.out.println("Sayı: " + islem2.sayi);
+	}
+}
