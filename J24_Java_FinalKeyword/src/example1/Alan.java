c51126450b007b4b760963c292b06133b225e1f7
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-15 01:36:16 +0300

    Final Keyword

---
+++ J24_Java_FinalKeyword/src/example1/Alan.java
@@ -0,0 +1,9 @@
+package example1;
+
+public class Alan {
+	public static void main(String[] args) {
+		Dikdortgen d1 = new Dikdortgen();
+		d1.b = 3; // Diğer kenar değiştirilebilir
+		d1.alanHesapla();
+	}
+}
---
+++ J24_Java_FinalKeyword/src/example1/Dikdortgen.java
@@ -0,0 +1,10 @@
+package example1;
+
+public class Dikdortgen {
+	final int a = 5; // Sabit değişken
+	int b;
+
+	public void alanHesapla() {
+		System.out.println("Alan: " + (a * b));
+	}
+}
