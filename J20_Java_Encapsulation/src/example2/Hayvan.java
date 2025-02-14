c92b0d2bdac8dca5bc669e629a539bcb273244c0
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-14 15:07:20 +0300

    Encapsulation

---
+++ J20_Java_Encapsulation/src/example2/Hayvan.java
@@ -0,0 +1,14 @@
+package example2;
+
+public class Hayvan {
+    private String isim;
+    private int boy;
+
+    private void kos() {
+        System.out.println("Koşmaktadır.");
+    }
+
+    private void avlan() {
+        System.out.println("Avlanmaktadır.");
+    }
+}
---
+++ J20_Java_Encapsulation/src/example2/Test.java
@@ -0,0 +1,10 @@
+package example2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Hayvan kedi = new Hayvan();
+//		kedi.isim = "Tekir"; // Hata: isim değişkenine erişim yok.
+//		kedi.kos(); // Hata: kos() metodu görünür değil.
+	}
+}
