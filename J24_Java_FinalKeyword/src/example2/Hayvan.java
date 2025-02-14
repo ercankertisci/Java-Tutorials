e1d05395238a81610c06ce5a6fbb474f7a9fbe1c
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-15 01:39:55 +0300

    Final Keyword

---
+++ J24_Java_FinalKeyword/src/example2/Hayvan.java
@@ -0,0 +1,7 @@
+package example2;
+
+public class Hayvan {
+	final void yaz() {
+		System.out.println("Ana sınıf metodu çağırıldı");
+	}
+}
---
+++ J24_Java_FinalKeyword/src/example2/Kopek.java
@@ -0,0 +1,8 @@
+package example2;
+
+public class Kopek extends Hayvan {
+    // HATA! Final metot override edilemez.
+//    void yaz() {
+//        System.out.println("Alt sınıf metodu çağırıldı");
+//    }
+}
