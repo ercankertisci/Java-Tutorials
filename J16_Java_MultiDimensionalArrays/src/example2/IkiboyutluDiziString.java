07830620604baebfb57c6e7edcc1e127088c9ee0
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-14 10:10:42 +0300

    MultiDimensional Arrays

---
+++ J16_Java_MultiDimensionalArrays/src/example2/IkiboyutluDiziString.java
@@ -0,0 +1,13 @@
+package example2;
+
+public class IkiboyutluDiziString {
+    public static void main(String[] args) {
+        String[][] isimler = {
+            {"Ahmet", "Mehmet"},
+            {"Erdem", "Umit"},
+            {"Ercan", "Mehmet"}
+        };
+
+        System.out.println(isimler[2][0] + " ve " + isimler[2][1]);
+    }
+}
