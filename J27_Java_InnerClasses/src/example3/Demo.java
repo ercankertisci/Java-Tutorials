b1b92208c9fdfc875326457b231dd27716ee23e5
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-25 13:48:22 +0300

    Inner Classes

---
+++ J27_Java_InnerClasses/src/example3/Demo.java
@@ -0,0 +1,8 @@
+package example3;
+
+public class Demo {
+	public static void main(String[] args) {
+		YerelSinifOrnegi nesne = new YerelSinifOrnegi();
+		nesne.metot();
+	}
+}
---
+++ J27_Java_InnerClasses/src/example3/YerelSinifOrnegi.java
@@ -0,0 +1,13 @@
+package example3;
+
+public class YerelSinifOrnegi {
+	void metot() {
+        class YerelSinif {
+            void mesaj() {
+                System.out.println("Ben bir yerel sınıfım!");
+            }
+        }
+        YerelSinif y1 = new YerelSinif();
+        y1.mesaj();
+    }
+}
