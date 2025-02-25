014f74f3dafe529c0c14ea46a1be25d9b0fe6337
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-25 13:47:13 +0300

    Inner Classes

---
+++ J27_Java_InnerClasses/src/example2/Demo.java
@@ -0,0 +1,9 @@
+package example2;
+
+public class Demo {
+	public static void main(String[] args) {
+        DisSinif disSinif = new DisSinif();
+        DisSinif.IcSinif icSinif = disSinif.new IcSinif();
+        icSinif.mesajGoster();
+    }
+}
---
+++ J27_Java_InnerClasses/src/example2/DisSinif.java
@@ -0,0 +1,11 @@
+package example2;
+
+public class DisSinif {
+	private String mesaj = "Merhaba, ben bir iç sınıfım!";
+
+	class IcSinif {
+		void mesajGoster() {
+			System.out.println(mesaj);
+		}
+	}
+}
