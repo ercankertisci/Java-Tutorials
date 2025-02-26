c96b0f8bdad258f1a11c72573fa7daa015b0d979
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-26 12:55:09 +0300

    String Class

---
+++ J29_Java_StringClass/src/example1/StringUzunluk.java
@@ -0,0 +1,9 @@
+package example1;
+
+public class StringUzunluk {
+	public static void main(String[] args) {
+		String metin = "Alexander Johnson";
+		int uzunluk = metin.length();
+		System.out.println("Uzunluk: " + uzunluk);
+	}
+}
