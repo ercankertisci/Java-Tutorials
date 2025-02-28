fe132dad7d44e2f683ca744a7fa98508481db3d7
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-28 10:55:20 +0300

    String Class

---
+++ J29_Java_StringClass/src/example9/StringBufferOrnek.java
@@ -0,0 +1,9 @@
+package example9;
+
+public class StringBufferOrnek {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("William").append(" Shakespeare ").append(20);
+		System.out.println(sb);
+	}
+}
