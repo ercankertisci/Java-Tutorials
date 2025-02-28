7d32f4e35065180f9b191255c055500d03601ff2
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-28 10:42:49 +0300

    String Class

---
+++ J29_Java_StringClass/src/example3/StringBirlestirme2.java
@@ -0,0 +1,10 @@
+package example3;
+
+public class StringBirlestirme2 {
+	public static void main(String[] args) {
+		String ad = "Liam";
+		String soyad = "Roberts";
+		String tamAd = ad.concat(" ").concat(soyad);
+		System.out.println(tamAd);
+	}
+}
