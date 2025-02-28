89a5f52603f45f313f9891bc89eb848b199e12e2
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-28 10:51:22 +0300

    String Class

---
+++ J29_Java_StringClass/src/example6/KarakterMetodlari.java
@@ -0,0 +1,10 @@
+package example6;
+
+public class KarakterMetodlari {
+	public static void main(String[] args) {
+		String kelime = "ProgrammingLanguage";
+
+		System.out.println("9. harf: " + kelime.charAt(8));
+		System.out.println("g harfi ilk nerede geçiyor: " + kelime.indexOf('g'));
+	}
+}
