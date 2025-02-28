f9d2cfc8530c81e09aa337aa44db86b578315460
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-28 10:39:38 +0300

    String Class

---
+++ J29_Java_StringClass/src/example2/StringBirlestirme.java
@@ -0,0 +1,10 @@
+package example2;
+
+public class StringBirlestirme {
+	public static void main(String[] args) {
+		String ad = "Sophia";
+		String soyad = "Anderson";
+		String tamAd = ad + " " + soyad;
+		System.out.println(tamAd);
+	}
+}
