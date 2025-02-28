861e25263b7d1aecce6b902609e634cc80a9b739
Ercan Kertisci <ercan.kertisci@gmail.com>
2025-02-28 15:24:33 +0300

    Exception Handling

--- J30_Java_ExceptionHandling/src/example6/CustomExceptionExample.java
+++ J30_Java_ExceptionHandling/src/example6/CustomExceptionExample.java
@@ -4,15 +4,16 @@ public class CustomExceptionExample {
 	static void checkNumber(int number) throws CustomException {
         if (number < 0) {
             throw new CustomException("Negatif sayılar kabul edilmez!");
-        } else {
-            System.out.println("Sayı: " + number);
-        }
-    }
-public static void main(String[] args) {
-    try {
-        checkNumber(-5);
-    } catch (CustomException e) {
-        System.out.println("Özel Hata Yakalandı: " + e.getMessage());
-    }
-}
+		} else {
+			System.out.println("Sayı: " + number);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			checkNumber(-5);
+		} catch (CustomException e) {
+			System.out.println("Özel Hata Yakalandı: " + e.getMessage());
+		}
+	}
 }
